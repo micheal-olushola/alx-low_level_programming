e9659b6b8fa5185d09b3409f5b1aa404dc1a090f
micheal-olushola <michealolusholatope@gmail.com>
2022-09-10 03:45:20 -0700

    c programing

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,16 +6,16 @@
  */
 int main(void)
 {
-	int l,u;
+int l, u;
 
-	for (l = 97; l <= 122; l++)
-	{
-		putchar(l);
-	}
-	for (u = 65; u <= 90; u++)
-	{
-		putchar(u);
-	}
-	putchar('\n');
-	return (0);
+for (l = 97; l <= 122; l++)
+{
+putchar(l);
+}
+for (u = 65; u <= 90; u++)
+{
+putchar(u);
+}
+putchar('\n');
+return (0);
 }
