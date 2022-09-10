00526eed460e1c659d8a21cdf920d9f669460af5
micheal-olushola <michealolusholatope@gmail.com>
2022-09-10 03:29:52 -0700

    c programing

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l,u;
+
+	for (l = 97; l <= 122; l++)
+	{
+		putchar(l);
+	}
+	for (u = 65; u <= 90; u++)
+	{
+		putchar(u);
+	}
+	putchar('\n');
+	return (0);
+}
