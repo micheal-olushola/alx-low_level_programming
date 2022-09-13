52570284d3730ae6252bedb54ac1da38ec3ae9f4
micheal-olushola <michealolusholatope@gmail.com>
2022-09-13 14:27:24 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,28 @@
-#include "main.h"
+#include "holberton.h"
+
+
+
 /**
- * print_alphabet_x10 - function to print abc 10 times
- * 
- * Return: 0
- */
+ *
+ *  * print_alphabet_x10 - func
+ *
+ *   *
+ *
+ *    * Return - 0
+ *
+ *     */
 void print_alphabet_x10(void)
 {
-char c, i;
-
-for (i = 0; i <= 9; i++)
-{
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
-}
-
+	int b = 0;
+	while (b < 10)
+	{
+		char a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		b++;
+	}
 }
