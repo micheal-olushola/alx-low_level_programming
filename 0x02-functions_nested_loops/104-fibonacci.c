78eb3ef07a8599a530cf538efa522182472ca117
micheal-olushola <michealolusholatope@gmail.com>
2022-09-13 16:24:53 -0700

    task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -20,19 +20,16 @@ int main(void)
 	unsigned long int aft2;
 	
 	printf("%lu", bef);
-
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
-	
 	bef1 = (bef / l);
 	bef2 = (bef % l);
 	aft1 = (aft / l);
 	aft2 = (aft % l);
-	
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", aft1 + (aft2 / l));
