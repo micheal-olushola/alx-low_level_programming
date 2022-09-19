786ab96abe5284e9f92bf11035514d79715d5e32
micheal-olushola <michealolusholatope@gmail.com>
2022-09-19 12:08:44 -0700

    c programing

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,21 @@
-#include "holberton.h"
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * reset_to_98 - main
- * @n: - pinter to int
- */
+* void prototypes()
+* int prototypes()
+*/
 
-void reset_to_98(int *n)
-{
-*n = 98;
-}
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
+
+#endif /*MAIN_H*/
